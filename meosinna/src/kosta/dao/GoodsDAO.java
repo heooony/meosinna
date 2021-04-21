64cd3a69061362ed3526120f3a5fb65d4af5db20
popp2002 <80496519+popp2002@users.noreply.github.com>
2021-04-21 18:08:33 +0900

    add dto, dao
    dao dto 추가

---
+++ meosinna/src/kosta/dao/GoodsDAO.java
@@ -0,0 +1,44 @@
+package kosta.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import kosta.dto.GoodsDTO;
+
+
+public interface GoodsDAO {
+	
+	/**
+	 * 전체검색
+	 * */
+	List<GoodsDTO> selectAll() throws SQLException;
+	
+	/**
+	 * 키워드 검색
+	 * */
+	List<GoodsDTO> selectByKeyword(String keyword) throws SQLException;
+	
+	/**
+	 * 좋아요 증가 기능???
+	 * */
+	int increamentByGdLike(String gdCode) throws SQLException;
+	
+	 /**
+	   * 레코드 삽입
+	   * @return : 1-삽입성공 , 0 - 삽입실패
+	   * */
+	  int insert(GoodsDTO goodsDTO) throws SQLException;
+	  
+	  /**
+	   * 모델번호에 해당하는 레코드 삭제
+	   * @return : 1-삭제성공 , 0 - 삭제실패
+	   * */
+	  int delete(String gdCode) throws SQLException;
+	  
+	   /**
+	    * 모델번호에 해당하는 레코드 수정
+	    * @return : 1-수정성공 , 0 - 수정실패
+	    * */
+	  int update(GoodsDTO goodsDTO) throws SQLException;
+	  
+}
